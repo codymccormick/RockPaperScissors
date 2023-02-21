6465cf6a9df069b4f4f274f8bc5f5bca29f4d3b5
Cody McCormick <mr.codymccormick@gmail.com>
2023-02-21 17:17:14 -0600

    added a little padding

--- javascript.js
+++ javascript.js
@@ -3,18 +3,17 @@ let computerScore = 0
 let winner
 const buttons = document.querySelectorAll('input')
 
+buttons.forEach(button =>{
+    button.addEventListener('click', function(){
+        playerChoice = button.value
+        playRound(button.value.toLowerCase())
+    })
+})
+
 function reload(winner){
     alert(`${winner} won`);
     location.reload();
 }
-
-
-buttons.forEach(button =>{
-    button.addEventListener('click', function(){
-        playerChoice = button.value
-        playRound(button.value)
-    })
-})
 
 function computerPlay() {
     let optionsList = ["rock", "paper", "scissors"];
@@ -29,25 +28,21 @@ function playRound(playerChoice, computerChoice) {
     if (playerChoice == computerChoice){
         result = (`It's a tie, you both chose ${playerChoice}` + `<br><br>Player score: ${playerScore} <br>Computer score: ${computerScore}`)  
     }
-
     //Win  Condition
     else if ((playerChoice == 'rock' && computerChoice == 'scissors') ||
     (playerChoice == 'scissors' && computerChoice == 'paper') ||
     (playerChoice == 'paper' && computerChoice == 'rock')) {
     playerScore++
     result = (`You won, ${playerChoice} beats ${computerChoice}` + `<br><br>Player score: ${playerScore} <br>Computer score: ${computerScore}`)
-
         if (playerScore == 5){
         winner = "you"
         reload(winner)
         } 
     }  
-
     //Lose Condition
     else {
     computerScore++
     result = (`You lost, ${computerChoice} beats ${playerChoice}` + `<br><br>Player score: ${playerScore} <br>Computer score: ${computerScore}`)  
-
         if (computerScore == 5){
             winner = 'computer'
         reload(winner)   
